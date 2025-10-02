c970299700673759e29d7a8f9a7338d7ccf03393
mohd-ksr <mohd.ksr2003@gmail.com>
2025-10-02 20:44:22 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants";
+// import express from "express"
+// const app = express()
+
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./.env"
+})
+
+import connectDB from "./db/index.js"
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+// ;(async () =>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) =>{
+//             console.error("ERROR: Unable to connect to database")
+//             throw error
+//         })
+//         app.listen(process.env.PORT, () =>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR:", error)
+//         throw error
+//     }
+// })
+        
