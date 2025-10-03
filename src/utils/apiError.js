1267dee8484c0be941f32742666b04fc70b4498e
mohd-ksr <mohd.ksr2003@gmail.com>
2025-10-03 14:17:23 +0530

    util code

---
+++ src/utils/apiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+    constructor(statusCode, message="something went wrong", errors=[], stack=""){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+        this.data=null
+        this.sucsess=false
+        if(stack){
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError };
