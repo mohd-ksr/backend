bd94030d9db71e323249893e2e4537e9bd44e0f8
mohd-ksr <mohd.ksr2003@gmail.com>
2025-10-04 17:16:25 +0530

    postman working

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,37 +3,56 @@ import {ApiError} from '../utils/apiError.js';
 import { User } from '../models/user.model.js';
 import { uploadToCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import fs from "fs"
 
 const registerUser = asyncHandler(async (req, res) => {
-        // get user data from frontend
-        // validation - not empty
-        // check if user already exists: email or username
-        // check for image, check avatar
-        // upload image to cloudinary, avatar
-        // create user object - create entry in db
-        // remove password and refresh token from response
-        // check for user creation
+    // get user data from frontend
+    // validation - not empty
+    // check if user already exists: email or username
+    // check for image, check avatar
+    // upload image to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token from response
+    // check for user creation
     // return response
     const {fullname, email, username, password} = req.body;
-    console.log("Email:", email);
+    // console.log("Email:", email);
 
     if([fullname, email, username, password].some((field) => field?.trim()==="")){
         throw new ApiError(400, "All fields are required");
     }
-    const existedUser = User.findOne({
+
+    const existedUser = await User.findOne({
         $or: [{ email },{ username }]
     })
+
     if(existedUser){
+        // cleanup uploaded files
+        if (req.files?.avatar?.[0]?.path) {
+            fs.unlinkSync(req.files.avatar[0].path);
+        }
+        if (req.files?.coverImage?.[0]?.path) {
+            fs.unlinkSync(req.files.coverImage[0].path);
+        }
         throw new ApiError(409, "User already exists with this email or username");
     }
+    
+    // console.log("Files:", req.files);
 
-    const avatarLocalPath = req.files?.avatar?.[0];
-    const coverImageLocalPath = req.files?.coverImage?.[0];
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required");
     }
+
     const avatar = await uploadToCloudinary(avatarLocalPath);
     const coverImage = await uploadToCloudinary(coverImageLocalPath);
+
     if(!avatar){
         throw new ApiError(500, "Unable to upload avatar");
     }
